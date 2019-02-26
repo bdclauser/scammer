965f34a405fe9030083602e5e8305b3aae8ef976
bdclauser <pzljug@hotmail.com>
2019-02-26 14:21:12 -0500

    initial commit

---
+++ scammer.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import random
+import string
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+# change url as needed
+url = 'https://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
+
+names = json.loads(open('package.json').read())
+
+for name in names:
+    name_extra = ''.join(random.choice(string.digits))
+
+    username = name.lower() + name_extra + '@yahoo.com'
+    password = ''.join(random.choice(chars) for i in range(8))
+
+    # sends to server
+    requests.post(url, allow_redirects=False, data={
+        'auid2yjauysd2uasdasdasd': username,
+        'kjauysd6sAJSDhyui2yasd': password
+    })
+
+    print('sending username %s and password %s' % (username, password))
